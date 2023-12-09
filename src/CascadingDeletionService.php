53cd0aa9b544846497f7d048c319c16002b1206c
Istvan Kadar <odesk.kadari@gmail.com>
2023-12-09 12:47:01 +0100

    Refactor deleteEntity method for better modularity
    - Split deleteEntity into smaller methods for improved readability
    - Added checkDeletionEligibility for pre-deletion eligibility checks
    - Created performDeletion for executing the actual deletion process
    - Updated inline documentation for clarity and accuracy

--- src/CascadingDeletionService.php
+++ src/CascadingDeletionService.php
@@ -3,25 +3,53 @@ namespace IKadar\CascadingDeletion;
 
 use IKadar\CascadingDeletion\DeletionTargetInterface;
 
+/**
+ * CascadingDeletionService handles the deletion of entities within a system,
+ * ensuring that both the primary entity and its dependent entities are eligible for deletion.
+ * This service maintains data integrity by performing a comprehensive check before deletion
+ * and aborting the process if any entity in the deletion chain cannot be deleted.
+ *
+ * Key functionalities include:
+ * - Collecting all entities to be deleted, including dependent entities.
+ * - Performing pre-deletion checks to ensure all entities are eligible for deletion.
+ * - Executing the deletion process for all collected entities.
+ */
 class CascadingDeletionService
 {
     /**
-     * Deletes the entity with the given ID using the provided repository.
+     * Initiates the deletion process for a given entity.
+     * This method orchestrates the process by collecting all dependent entities,
+     * checking their eligibility for deletion, and then, if eligible, performing the deletion.
      *
-     * @param DeletionTargetInterface $topTarget
-     */
-    public function deleteEntity(DeletionTargetInterface $topTarget): void
+     * @param DeletionTargetInterface $topTarget The top-level entity target for deletion.
+     */    public function deleteEntity(DeletionTargetInterface $topTarget): void
     {
         // Collects the IDs of all entities to be deleted
         $deletionTargets = $this->collectDeletionTargets($topTarget);
 
         // Perform pre-deletion check all collected targets
-        foreach ($deletionTargets as $deletionTarget) {
+        if ($this->checkDeletionEligibility($deletionTargets, $topTarget)) {
 
+            // Perform deletion on all collected targets
+            $this->performDeletion($deletionTargets);
+            echo "All entities deleted successfully.\n";
+        }
+    }
+
+    /**
+     * Checks if all collected entities are eligible for deletion.
+     * If any entity is found to be undeletable, the process is aborted and a message is output.
+     *
+     * @param array $deletionTargets Array of entities to be deleted.
+     * @param DeletionTargetInterface $topTarget The top-level entity target for deletion.
+     * @return bool Returns true if all entities are deletable, false otherwise.
+     */
+    private function checkDeletionEligibility(array $deletionTargets, DeletionTargetInterface $topTarget): bool
+    {
+        foreach ($deletionTargets as $deletionTarget) {
             $unDeletableTargets = $deletionTarget->getUnDeletableTargets($deletionTargets);
 
             if ($unDeletableTargets !== []) {
-
                 $topTarget->setIsDeletable(false);
                 $topTarget->setMessage("Top level message");
                 $unDeletableTargets[] = $topTarget;
@@ -32,22 +60,31 @@ class CascadingDeletionService
                 );
 
                 var_dump($unDeletableTargets);
-                return;
+                return false;
             }
         }
-
-        // Perform deletion on all collected targets
-        foreach ($deletionTargets as $deletionTarget) {
-            $deletionTarget->getRepository()->performDelete($deletionTarget->getEntityId());
-        }
-
-        echo "All entities deleted successfully.\n";
+        return true;
     }
 
     /**
-     * Collects the IDs of all entities that should be deleted.
+     * Performs the deletion of all eligible entities.
+     * This method is called after verifying that all entities in the deletion list are deletable.
      *
-     * @param DeletionTargetInterface $target
+     * @param array $deletionTargets Array of entities to be deleted.
+     */
+    private function performDeletion(array $deletionTargets): void
+    {
+        foreach ($deletionTargets as $deletionTarget) {
+            $deletionTarget->getRepository()->performDelete($deletionTarget->getEntityId());
+        }
+    }
+
+    /**
+     * Collects all entities that are to be deleted along with the given entity.
+     * This method uses a recursive approach to identify and collect all dependent
+     * entities that should be deleted.
+     *
+     * @param DeletionTargetInterface $target The entity from which to start collecting deletion targets.
      * @return DeletionTarget[] An array of DeletionTarget objects representing entities to be deleted.
      */
     private function collectDeletionTargets(DeletionTargetInterface $target): array
@@ -58,14 +95,15 @@ class CascadingDeletionService
     }
 
     /**
-     * Helper method to recursively collect deletion targets.
+     * A helper method to recursively collect deletion targets.
+     * It ensures that each entity is only added once to the list of targets
+     * and recursively collects targets from entities referenced by the current target.
      *
-     * @param DeletionTargetInterface $target
-     * @param DeletionTargetInterface[] $targets
+     * @param DeletionTargetInterface $target The current entity being processed.
+     * @param DeletionTargetInterface[] $targets Reference to an array of already collected targets.
      */
     private function collect(DeletionTargetInterface $target, array &$targets): void
     {
-
         // Avoid re-adding the same entity
         if (isset($targets[$target->getEntityId()])) {
             return;
